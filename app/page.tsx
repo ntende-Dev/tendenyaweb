ccf295502eb94b73272581d0ae0d89b62e991025
ntende-Dev <tendenya@me.com>
2023-02-13 18:55:04 +0200

    the built before build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import About from "./components/About/page";
-import { Contact } from "./components/Contact/page";
+"use client";
 import Home from "./components/Home/HomePage";
-import Services from "./components/Services/page";
 
 export default function HomePage() {
   return (
