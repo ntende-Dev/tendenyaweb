802035c17ad9a4740ce23bdc4b6b09baffbac6a9
ntende-Dev <tendenya@me.com>
2023-02-13 20:14:41 +0200

    added page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
-"use client";
-import Home from "./components/Home/HomePage";
+import Image from "next/image";
+import Link from "next/link";
+import Hero from "../app/HeroImage.png";
+import h from "../app/components/Home/Home.module.scss";
 
-export default function HomePage() {
+const Home = () => {
   return (
     <>
-      <Home />
+      <main className="container">
+        <div className={h.homeWrapper}>
+          <div className={h.lefty}>
+            <p className="breadCrumb">The home of</p>
+            <h1 className={h.title}>
+              <span className={h.spani}> Masterfully </span>
+              <br />
+              crafted logos & <br />
+              <span className={h.spani}> responsive</span> websites!
+            </h1>
+            <p className={h.para}>
+              Get a logo which is optimized for brand visibility <br />
+              and a web application which is accessible, secure & fast!
+              <br />
+              <br />
+              Contact us for cutting edge & innovative digital solutions!
+            </p>
+            <Link href={"/"}>
+              <button className={h.contactBtn}>Request a quote</button>
+            </Link>
+          </div>
+
+          <div className={h.righty}>
+            <Image src={Hero} alt="heroImage" />
+          </div>
+        </div>
+      </main>
     </>
   );
-}
+};
+
+export default Home;
